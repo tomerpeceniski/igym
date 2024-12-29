daa9700477aceb6b0c6b5efa68c5774c5442f0d8
Tomer Peceniski <tomerpeceniski@gmail.com>
2024-12-29 16:49:50 +0200

    creating application

---
+++ src/main/java/igym/application/IgymApplication.java
@@ -0,0 +1,13 @@
+package igym.application;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IgymApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IgymApplication.class, args);
+	}
+
+}
---
+++ src/test/java/igym/application/IgymApplicationTests.java
@@ -0,0 +1,13 @@
+package igym.application;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IgymApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
