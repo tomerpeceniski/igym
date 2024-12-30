374cb363b207a15f354e91e36e5c674d3003e75e
Tomer Peceniski <tomerpeceniski@gmail.com>
2024-12-30 15:34:27 +0200

    Approved Unity Test for UserRepository

---
+++ src/test/java/igym/repositories/UserRepositoryTest.java
@@ -0,0 +1,30 @@
+package igym.repositories;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+import java.util.Optional;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import igym.entities.User;
+
+@SpringBootTest
+public class UserRepositoryTest {
+
+    @Autowired 
+    UserRepository repository;
+
+    @Test
+    public void saveAndFindTest() {
+        User user = new User(null, "John Snow");
+        User savedUser = repository.save(user);
+        assertNotNull(savedUser.getId());
+
+        Optional<User> optional = repository.findById(savedUser.getId());
+        User findedUser = optional.get();
+        assertEquals(findedUser, savedUser);
+    }
+}
