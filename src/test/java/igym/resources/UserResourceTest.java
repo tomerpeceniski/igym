c81b93cfbbb23246da58dbd56d49e0c91c83b772
Tomer Peceniski <tomerpeceniski@gmail.com>
2025-01-02 19:28:03 +0200

    Aproved UserResourceTest

--- src/test/java/igym/resources/UserResourceTest.java
+++ src/test/java/igym/resources/UserResourceTest.java
@@ -1,5 +1,33 @@
 package igym.resources;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import java.util.List;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+
+import igym.entities.User;
+import igym.services.UserService;
+
+@SpringBootTest
 public class UserResourceTest {
 
+    @Autowired
+    UserResource resource;
+
+    @Autowired
+    UserService service;
+
+    @Test
+    public void findAllTest() {
+        List<User> listUsers = service.findAll();
+
+        ResponseEntity<List<User>> response = resource.findAll();
+        assertEquals(listUsers, response.getBody());
+        assertEquals(HttpStatus.OK, response.getStatusCode());
+    }
 }
