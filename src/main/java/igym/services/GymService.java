4ecee1a1b1aaa4075bbd221c7915e2ebe3b2ca59
Daniel Sender <70211437+daniel1sender@users.noreply.github.com>
2025-01-09 18:42:13 +0200

    remove unecessary annotation

--- src/main/java/igym/services/GymService.java
+++ src/main/java/igym/services/GymService.java
@@ -10,15 +10,14 @@ import igym.entities.*;
 
 @Service
 public class GymService {
+    
     @Autowired
     private GymRepository gymRepository;
 
-    @Autowired
     public GymService(GymRepository gymRepository) {
         this.gymRepository = gymRepository;
     }
 
-    @Autowired
     public List<Gym> findAllGyms() {
         return gymRepository.findAll();
     }
