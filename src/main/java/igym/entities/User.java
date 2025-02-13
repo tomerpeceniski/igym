e1283ffb6decff804622a75d17476aa5541e242a
Tomer Peceniski <tomerpeceniski@gmail.com>
2025-02-13 19:13:33 +0200

    Add validation constraints for User Name

--- src/main/java/igym/entities/User.java
+++ src/main/java/igym/entities/User.java
@@ -3,6 +3,7 @@ package igym.entities;
 import java.util.UUID;
 
 import jakarta.persistence.*;
+import jakarta.validation.constraints.*;
 
 @Table(name = "users")
 @Entity
@@ -11,6 +12,10 @@ public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.UUID)
     private UUID id;
+
+    @Column(nullable = false, length = 50)
+    @NotBlank(message = "Name cannot be blank")
+    @Size(max = 50, message = "Name cannot have more than 50 characters")
     private String name;
 
     public User() {
