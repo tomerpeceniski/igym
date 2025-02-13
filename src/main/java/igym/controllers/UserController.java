0b5622458ecd23b50f9efb519bb6a2c5bc21c34f
Tomer Peceniski <tomerpeceniski@gmail.com>
2025-02-13 20:23:30 +0200

    update annotation for correct functioning

--- src/main/java/igym/controllers/UserController.java
+++ src/main/java/igym/controllers/UserController.java
@@ -11,13 +11,16 @@ import java.util.List;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.PostMapping;
+
 
 @RestController
 @RequestMapping(value = "/users")
+@Validated
 public class UserController {
 
     private final UserService service;
@@ -32,8 +35,8 @@ public class UserController {
         return ResponseEntity.ok().body(list);
     }
 
-    @PutMapping(produces = MediaType.APPLICATION_JSON_VALUE)
-    public ResponseEntity<User> createUser(@Valid @RequestBody User user) {
+    @PostMapping(produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<User> createUser(@RequestBody @Valid User user) {
         User savedUser = service.createUser(user);
         return ResponseEntity.status(HttpStatus.CREATED).body(savedUser);
     }
