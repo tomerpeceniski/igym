01e6ce6c57973d8778cb302c7ba82655453403d7
Tomer Peceniski <tomerpeceniski@gmail.com>
2025-02-14 10:29:01 +0200

    Test User Constraints

--- src/test/java/igym/entities/UserTest.java
+++ src/test/java/igym/entities/UserTest.java
@@ -2,25 +2,37 @@ package igym.entities;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import java.util.Set;
+
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import jakarta.validation.*;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+
 public class UserTest {
+
+    Validator validator;
+
+    @BeforeEach
+    public void config() {
+        ValidatorFactory factory = Validation.buildDefaultValidatorFactory();
+        validator = factory.getValidator();
+        }
+
 
     @Test
     @DisplayName("Getting user name")
     public void userNameTest() {
-        String name1 = "Maria Clown";
-        User user1 = new User();
-        user1.setName(name1);
-        assertEquals(user1.getName(), name1);
+        String userName = "Maria Clown";
+        User user = new User();
+        user.setName(userName);
+        assertEquals(user.getName(), userName);
 
-        String name2 = "John Textor";
-        User user2 = new User(name2);
-        assertEquals(user2.getName(), name2);
-
-        User user3 = new User();
-        assertNull(user3.getName());
+        Set<ConstraintViolation<User>> violations = validator.validate(user);
+        assertTrue(violations.isEmpty());
     }
 
     @Test
@@ -29,5 +41,41 @@ public class UserTest {
         String name1 = "Maria Clown";
         User user1 = new User(name1);
         assertNull(user1.getId(), "When a User is created, the ID should be null");
+    }
+
+    @Test
+    @DisplayName("Testing null constraint")
+    public void nullConstraintTest() {
+        User nullNameUser = new User();
+        Set<ConstraintViolation<User>> violations = validator.validate(nullNameUser);
+        assertTrue(violations.size() == 1);
+        ConstraintViolation<User> violation = violations.iterator().next();
+        assertEquals(NotBlank.class, violation.getConstraintDescriptor().getAnnotation().annotationType());
+    }
+
+    @Test
+    @DisplayName("Testing blank constraint")
+    public void blankConstraintTest() {
+        User blankNameUser = new User("");
+        Set<ConstraintViolation<User>> violations = validator.validate(blankNameUser);
+        assertTrue(violations.size() == 1);
+        ConstraintViolation<User> violation = violations.iterator().next();
+        assertEquals(NotBlank.class, violation.getConstraintDescriptor().getAnnotation().annotationType());
+
+        blankNameUser = new User(" ");
+        violations = validator.validate(blankNameUser);
+        assertTrue(violations.size() == 1);
+        violation = violations.iterator().next();
+        assertEquals(NotBlank.class, violation.getConstraintDescriptor().getAnnotation().annotationType());
+    }
+
+    @Test
+    @DisplayName("Testing maximum characters constraint")
+    public void maximumCharacterConstraintTest() {
+        User bigCharacterUser = new User("a".repeat(51));
+        Set<ConstraintViolation<User>> violations = validator.validate(bigCharacterUser);
+        assertTrue(violations.size() == 1);
+        ConstraintViolation<User> violation = violations.iterator().next();
+        assertEquals(Size.class, violation.getConstraintDescriptor().getAnnotation().annotationType());
     }
 }
