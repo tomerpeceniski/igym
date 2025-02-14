6b5ba04a13869a2b31b23e6f9bae0ed084e54e8b
Tomer Peceniski <tomerpeceniski@gmail.com>
2025-02-14 10:58:21 +0200

    Test UserService

--- src/test/java/igym/services/UserServiceTest.java
+++ src/test/java/igym/services/UserServiceTest.java
@@ -6,21 +6,22 @@ import static org.mockito.Mockito.*;
 import java.util.ArrayList;
 import java.util.List;
 
-import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import igym.entities.User;
 import igym.repositories.UserRepository;
+import igym.services.exceptions.DuplicateUserException;
 
 public class UserServiceTest {
 
     private UserRepository repository = mock(UserRepository.class);
     private UserService service = new UserService(repository);
-    static List<User> users;
+    private List<User> users;
 
-    @BeforeAll
-    public static void configurations() {
+    @BeforeEach
+    public void configurations() {
         users = new ArrayList<>();
         users.add(new User("Maria Clown"));
         users.add(new User("John Textor"));
@@ -30,10 +31,8 @@ public class UserServiceTest {
     @DisplayName("Test getting users")
     public void findAllTest() {
         when(repository.findAll()).thenReturn(users);
-
         List<User> listUsers = service.findAll();
-
-        assertEquals(listUsers, users);
+        assertIterableEquals(listUsers, users);
     }
 
     @Test
@@ -44,4 +43,20 @@ public class UserServiceTest {
         assertTrue(list.isEmpty());
     }
 
+    @Test
+    @DisplayName("Test create user")
+    public void createUserTest() {
+        when(repository.save(users.get(0))).thenReturn(users.get(0));
+        User savedUser = service.createUser(users.get(0));
+        assertEquals(users.get(0), savedUser);
+        verify(repository, times(1)).save(users.get(0));
+    }
+
+    @Test
+    @DisplayName("Test create duplicate user")
+    public void createDuplicateUserTest() {
+        when(repository.existsByName(users.get(0).getName())).thenReturn(true);
+        assertThrows(DuplicateUserException.class, () -> service.createUser(users.get(0)));
+        verify(repository, never()).save(users.get(0));
+    }
 }
