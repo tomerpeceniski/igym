5df40a9fa7279a71860ce867375000edfb5be3c0
Tomer Peceniski <tomerpeceniski@gmail.com>
2025-03-27 15:42:40 +0200

    feat: merge new features from main branch

--- src/test/java/igym/services/UserServiceTest.java
+++ src/test/java/igym/services/UserServiceTest.java
@@ -5,12 +5,15 @@ import static org.mockito.Mockito.*;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import igym.entities.User;
+import igym.entities.enums.Status;
+import igym.exceptions.UserNotFoundException;
 import igym.exceptions.DuplicateUserException;
 import igym.repositories.UserRepository;
 
@@ -44,6 +47,33 @@ public class UserServiceTest {
     }
 
     @Test
+    @DisplayName("Should delete user from repository")
+    public void deleteUserTest() {
+        User user = users.get(0);
+        when(repository.findById(user.getId())).thenReturn(Optional.of(user));
+        service.deleteUser(user.getId());
+        assertEquals(user.getStatus(), Status.inactive);
+    }
+
+    @Test
+    @DisplayName("Should throw exception when trying to delete inexistent user")
+    public void deleteNonExistentUserTest() {
+        User user = users.get(0);
+        when(repository.findById(user.getId())).thenReturn(Optional.empty());
+        assertThrowsExactly(UserNotFoundException.class, () -> service.deleteUser(user.getId()));
+        verify(repository, never()).save(user);
+    }
+
+    @Test
+    @DisplayName("Should throw exception when trying to delete already deleted user")
+    public void deleteDeletedUserTest() {
+        User user = users.get(0);
+        user.setStatus(Status.inactive);
+        when(repository.findById(user.getId())).thenReturn(Optional.of(user));
+        assertThrowsExactly(UserNotFoundException.class, () -> service.deleteUser(user.getId()));
+        verify(repository, never()).save(user);
+    }
+
     @DisplayName("Should return a list that contains the saved user")
     public void createUserTest() {
         when(repository.save(users.get(0))).thenReturn(users.get(0));
