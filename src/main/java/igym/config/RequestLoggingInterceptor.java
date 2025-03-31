7fda99156b434e415ba15afc474e34958b1b9fd0
Tomer Peceniski <tomerpeceniski@gmail.com>
2025-03-31 10:14:00 +0300

    refactor: hide path variables in request logs

--- src/main/java/igym/config/RequestLoggingInterceptor.java
+++ src/main/java/igym/config/RequestLoggingInterceptor.java
@@ -6,23 +6,31 @@ import org.springframework.lang.NonNull;
 import org.springframework.lang.Nullable;
 import org.springframework.stereotype.Component;
 import org.springframework.web.servlet.HandlerInterceptor;
+import org.springframework.web.servlet.HandlerMapping;
 
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
 @Component
 public class RequestLoggingInterceptor implements HandlerInterceptor {
-    
+
     private static final Logger logger = LoggerFactory.getLogger(RequestLoggingInterceptor.class);
 
     @Override
-    public boolean preHandle(@NonNull HttpServletRequest request, @NonNull HttpServletResponse response, @NonNull Object handler) {
-        logger.info("Received {} {}", request.getMethod(), request.getRequestURI());
+    public boolean preHandle(@NonNull HttpServletRequest request, @NonNull HttpServletResponse response,
+            @NonNull Object handler) {
+        String pattern = (String) request.getAttribute(HandlerMapping.BEST_MATCHING_PATTERN_ATTRIBUTE);
+        logger.info("RequestLoggingInterceptor - Received {} {}", request.getMethod(), pattern);
+        request.setAttribute("startTime", System.currentTimeMillis());
         return true;
     }
 
     @Override
-    public void afterCompletion(@NonNull HttpServletRequest request, @NonNull HttpServletResponse response, @NonNull Object handler, @Nullable Exception ex) {
-        logger.info("Responding {} {} - status [{}]", request.getMethod(), request.getRequestURI(), response.getStatus());
+    public void afterCompletion(@NonNull HttpServletRequest request, @NonNull HttpServletResponse response,
+            @NonNull Object handler, @Nullable Exception ex) {
+        String pattern = (String) request.getAttribute(HandlerMapping.BEST_MATCHING_PATTERN_ATTRIBUTE);
+        long duration = System.currentTimeMillis() - (Long) request.getAttribute("startTime");
+        logger.info("RequestLoggingInterceptor - Responding {} {} - status [{}] - {} ms",
+                request.getMethod(), pattern, response.getStatus(), duration);
     }
 }
