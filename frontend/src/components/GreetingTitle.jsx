167a8291b1edded19924994d010dfe849b41c334
Tomer Peceniski <tomerpeceniski@gmail.com>
2025-04-22 21:31:51 +0300

    Feat: create GreetingsTitle component

---
+++ frontend/src/components/GreetingTitle.jsx
@@ -0,0 +1,9 @@
+import Typography from '@mui/material/Typography';
+
+export default function GreetingTitle() {
+  return (
+    <Typography variant="h4" component="h1">
+      Hello User!
+    </Typography>
+  );
+}
---
+++ frontend/src/pages/home.jsx
@@ -0,0 +1,9 @@
+import GreetingTitle from '../components/GreetingTitle.jsx';
+
+export default function GymsPage() {
+  return (
+    <div>
+      <GreetingTitle />
+    </div>
+  );
+}
