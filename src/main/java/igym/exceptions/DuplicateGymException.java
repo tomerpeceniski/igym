3561372c9abf83abac139447c56dbc39ee135b4e
Tomer Peceniski <tomerpeceniski@gmail.com>
2025-04-29 12:28:08 +0300

    feat: add Javadoc for Exceptions and ExceptionHandler

--- src/main/java/igym/exceptions/DuplicateGymException.java
+++ src/main/java/igym/exceptions/DuplicateGymException.java
@@ -1,5 +1,8 @@
 package igym.exceptions;
 
+/**
+ * Thrown when attempting to create or update a gym with fields that already exist for the current user.
+ */
 public class DuplicateGymException extends RuntimeException {
     public DuplicateGymException(String message) {
         super(message);
