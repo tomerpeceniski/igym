c7be749b187f7942bd339705f3d7d41dd33bd09b
Tomer Peceniski <tomerpeceniski@gmail.com>
2025-04-29 14:18:52 +0300

    feat: add Javadoc for classes of repositories package

--- src/main/java/igym/repositories/UserRepository.java
+++ src/main/java/igym/repositories/UserRepository.java
@@ -7,8 +7,24 @@ import org.springframework.lang.NonNull;
 
 import igym.entities.User;
 
+/**
+ * Repository interface for managing {@link User} entities.
+ * Provides methods to perform CRUD operations.
+ */
 public interface UserRepository extends JpaRepository<User, UUID> {
+    /**
+     * Checks if a user with the specified ID exists.
+     *
+     * @param id the UUID of the user
+     * @return true if a user with the given ID exists, false otherwise
+     */
     public boolean existsById(@NonNull UUID id);
 
+    /**
+     * Checks if a user with the specified name exists.
+     *
+     * @param name the name of the user
+     * @return true if a user with the given name exists, false otherwise
+     */
     public boolean existsByName(String name);
 }
