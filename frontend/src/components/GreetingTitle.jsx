07faceb6eec631364304dba5237a047dafc3f578
Tomer Peceniski <tomerpeceniski@gmail.com>
2025-05-05 14:44:25 +0300

    refactor: improve greeting title vizualization

--- frontend/src/components/GreetingTitle.jsx
+++ frontend/src/components/GreetingTitle.jsx
@@ -3,8 +3,8 @@ import logo from '../assets/igym-logo.png';
 
 export default function GreetingTitle() {
   return (
-    <Box display="flex" alignItems="center" gap={2} sx={(theme) => ({ height: theme.spacing(8) })}>
-      <Box component="img" src={logo} alt="iGym Logo" sx={{ height: '100%' }} />
+    <Box display="flex" alignItems="center" gap={2} >
+      <Box component="img" src={logo} alt="iGym Logo" sx={(theme) => ({ height: theme.spacing(8) })} />
       <Typography
         variant="h3"
         component="h1"
