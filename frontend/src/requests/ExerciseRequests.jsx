e4b21924f688133618ee7b59409fca41947c6b7d
Tomer Peceniski <tomerpeceniski@gmail.com>
2025-05-13 09:51:23 +0300

    refactor: add environmental variables for api base url

--- frontend/src/requests/ExerciseRequests.jsx
+++ frontend/src/requests/ExerciseRequests.jsx
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_BASE_URL = '/api/v1';
+const API_BASE_URL =  import.meta.env.VITE_API_BASE_URL;
 
 export const deleteExercise = async (exerciseId) => {
   const response = await axios.delete(`${API_BASE_URL}/exercises/${exerciseId}`);
--- frontend/src/requests/GymRequests.jsx
+++ frontend/src/requests/GymRequests.jsx
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_BASE_URL = '/api/v1';
+const API_BASE_URL =  import.meta.env.VITE_API_BASE_URL;
 
 export const getGymsByUserId = (userId) => {
   return axios.get(`${API_BASE_URL}/users/${userId}/gyms`);
