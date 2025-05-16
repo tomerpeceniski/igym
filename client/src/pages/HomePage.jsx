b9542a59133e4ac28be811439e361648565e9b3b
Daniel Sender <70211437+daniel1sender@users.noreply.github.com>
2025-05-16 11:34:57 +0300

    Refactor: remove unused React import in HomePage component

--- client/src/pages/HomePage.jsx
+++ client/src/pages/HomePage.jsx
@@ -1,4 +1,3 @@
-import React from 'react';
 import { Box, Typography } from '@mui/material';
 import GreetingTitle from '../components/GreetingTitle';
 import GymHeader from '../components/GymHeader';
