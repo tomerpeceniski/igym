1a884fb225c32bb7b301980083903058c2d810b9
Daniel Sender <70211437+daniel1sender@users.noreply.github.com>
2025-05-23 17:51:08 +0300

    Fix: remove unnecessary whitespace in axios request interceptor for consistency

--- client/src/requests/axiosInstance.js
+++ client/src/requests/axiosInstance.js
@@ -8,7 +8,7 @@ const axiosInstance = axios.create({ baseURL });
 
 axiosInstance.interceptors.request.use(
   (config) => {
-    const token = localStorage.getItem('token');
+    const token = localStorage.getItem('token');  
     if (token) {
       config.headers['Authorization'] = `Bearer ${token}`;
     }
