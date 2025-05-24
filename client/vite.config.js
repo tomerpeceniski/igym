7e3a1274b15daa7ec6763427292f33170ff8b56b
Tomer Peceniski <tomerpeceniski@gmail.com>
2025-05-24 09:55:12 +0300

    refactor: add cors config para local tests

--- client/vite.config.js
+++ client/vite.config.js
@@ -2,14 +2,14 @@ import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig(({ mode }) => {
-  const env = loadEnv(mode, process.cwd(), 'VITE_');
-
+  const env = loadEnv(mode, process.cwd(), '');
+  const backendHost = env.VITE_API_BASE;
   return {
     plugins: [react()],
     server: {
       proxy: {
         '/api': {
-          target: env.VITE_API_BASE,
+          target: backendHost,
           changeOrigin: true,
           secure: false,
         },
